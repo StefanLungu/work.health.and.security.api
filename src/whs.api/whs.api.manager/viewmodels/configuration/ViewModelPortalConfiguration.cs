e674139381137ab1f0b98f933915502a823640a6
Stefan Lungu <stefan.lungu0227@gmail.com>
2024-06-14 21:31:43 +0300

    implement portal configuration

---
+++ src/whs.api/whs.api.manager/viewmodels/configuration/ViewModelPortalConfiguration.cs
@@ -0,0 +1,8 @@
+﻿namespace whs.api.manager.viewmodels.configuration;
+
+public class ViewModelPortalConfiguration
+{
+    public required string ApiOriginUrl { get; init; }
+    public required int DropDownPageSize { get; init; }
+    public required int GridPageSize { get; init; }
+}
